36c3af11da77712529f020e4628edee57d4a24bf
HP <HP@DESKTOP-5AC0PRQ>
2021-07-09 18:07:15 +0300

    Hello World

--- src/javaapplication456/JavaApplication456.java
+++ src/javaapplication456/JavaApplication456.java
@@ -1,124 +1,124 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
-* https://twitter.com/ea_plus/status/1409844229753483264
-* https://www.facebook.com/permalink.php?story_fbid=1306067539787545&id=100011529331580
-* https://www.facebook.com/mutetimaurice/posts/2866567980274193
-* https://www.linkedin.com/posts/learning-tutorials-731a29114_how-to-install-windows-11-activity-6815613787006226432-PKAx
-* https://slashdot.org/submission/14081432/how-to-install-windows-11
-* https://www.linkedin.com/pulse/how-install-windows-11-learning-tutorials/
-* https://www.pinterest.com/pin/94505292172173849
-* https://www.instapaper.com/read/1424271694
-* https://www.instapaper.com/read/1424271834
-* https://dai.ly/x82bx57
-* https://neolatino.ning.com/profiles/blogs/how-to-install-windows-11
-* https://neolatino.ning.com/profiles/status/show?id=516621%3AStatus%3A23576934
-* https://neolatino.ning.com/video/how-to-install-windows-11
-* https://vimeo.com/568893449
-* https://www.dailymotion.com/video/x82bx57
-* http://www.socialbookmarkssite.com/bookmark/3069086/how-to-install-windows-11/
-* http://www.bookmark4you.com/user/2976279-mauricemuteti2015
-* http://www.video-bookmark.com/bookmark/4474984/how-to-install-windows-11/
-* http://www.socialbookmarkssite.com/bookmark/3069120/how-to-install-windows-11/
-* http://www.video-bookmark.com/bookmark/4475003/how-to-install-windows-11/
-* https://vk.com/id661634275?w=wall661634275_1
-* https://vk.com/wall661634275_1
-* https://vk.com/id661634275
-* https://mauricemuteti.skyrock.com/3342974228-How-To-Install-Windows-11.html
-* https://www.reddit.com/user/Sdreloaded/comments/oa8quh/how_to_install_windows_11/
-* https://www.taringa.net/+videos/how-to-install-windows-11-article_4483i0
-* https://www.minds.com/newsfeed/1256613515154817024?referrer=mauricemuteti
-* https://www.gaiaonline.com/profiles/mmaurixe/45550952/
-* https://mauricemutetingundi.blogspot.com/2021/06/how-to-install-windows-11.html
-* https://mauricemuteti2015.tumblr.com/post/655336267607834624/how-to-install-windows-11-maurice-muteti
-* https://mauricemutetingundi.wordpress.com/2021/06/29/how-to-install-windows-11-maurice-muteti/
-* https://mauricemuteti2015.medium.com/how-to-install-windows-11-f170cc47999c
-* https://b.hatena.ne.jp/entry/panel/?url=* https%3A%2F%2Fmauricemuteti.info%2Fhow-to-install-windows-11%2F%23.YNtpPxBI5W4.hatena&btitle=How+To+Install+Windows+11+%E2%80%93+Maurice+Muteti
-* https://b.hatena.ne.jp/entry/4704771760354497602/comment/mauricemuteti2015
-* https://b.hatena.ne.jp/entry/s/mauricemuteti.info/how-to-install-windows-11/
-* https://www.folkd.com/user/mauricemuteti2015
-* https://www.folkd.com/user/mauricemuteti2015?filter_content=news&pagenum=1
-* https://www.folkd.com/user/mauricemuteti2015?filter_content=pages&pagenum=1
-* https://www.scoop.it/topic/how-to-tutorials/p/4125588780/2021/06/29/* https-mauricemuteti-info-how-to-install-windows-11
-* http://sco.lt/9CRFWC
-* https://twitter.com/ea_plus/status/1409960403250130945
-* https://www.pinterest.com/pin/94505292172175653
-* https://www.facebook.com/permalink.php?story_fbid=1306318636429102&id=100011529331580
-* https://mix.com/!uzQxFncQ:how-to-install-windows-11
-* https://mix.com/!H8Ede87J:* https%3A-mauricemuteti.info-how-to-install-windows-11
-* https://twitter.com/ea_plus/status/1409962829696864260
-* https://www.pinterest.com/pin/94505292172175691
-* https://nk.pl/#profil/53355945/
-* https://www.bibsonomy.org/url/7b3f6fb68f59f5f93618391a6b4de543
-* https://www.bookmarky.cz/users/mauricemuteti
-* https://www.diigo.com/user/mauricemuteti
-* https://diigo.com/0l1ywz
-* https://www.diigo.com/annotated/2e09530c3da71c3363f021e97f28a211
-* https://www.liveinternet.ru/users/mauricemuteti/post485154054//
-* https://www.pusha.se/how-to-install-windows-11
-* https://www.pusha.se/how-to-install-windows-11-2
-* https://www.plurk.com/p/og0i2r
-* https://www.plurk.com/p/og0i4f
-* https://flip.it/b0Eyjx
-* https://www.google.com/url?q=* https://mauricemuteti.info/how-to-install-windows-11/%23.YN8A2PR8aN0.google&usd=2&usg=AFQjCNEQsIp3BMNaJliZuxGUSdrOwROr7g
-* https://sites.google.com/site/mauricemutetingundi2015/how-to-install-windows-11
-* https://b.hatena.ne.jp/entry?url=* https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DiRt5VnxSA6o
-* https://mauricemuteti.livejournal.com/1787.html
-* https://www.evernote.com/shard/s581/sh/9260e90c-34f5-fa68-1c84-326141d126fc/38e464129bf93124e14cd5aa2abb8c7c
-* https://posteezy.com/how-install-windows-11-0
-* https://digg.com/news/link/how-to-install-windows-11-xPxRrhDZq1
-* https://digg.com/@maurice-muteti
-* https://www.smore.com/yqutg-my-smore-newsletter
-* http://mauricemuteti.zohosites.com/blogs/post/how-to-install-windows-11
-* https://mauricemuteti.page.tl/How-To-Install-Windows-11.htm
-* https://unknowzeros-website.yolasite.com/how-to-install-windows-11/
-* https://mauricemuteti.webs.com/apps/blog/
-* https://mauricemuteti.webs.com/apps/blog/show/49924387-how-to-install-windows-11
-* https://mauricemuteti.shutterfly.com/howtoinstallwindows11
-* https://mauricemuteti.shutterfly.com/howtoinstallwindows11/2
-* https://mauricemuteti.shutterfly.com/howtoinstallwindows11/3
-* https://mauricemuteti.webnode.com/accommodation/
-* https://unknownzero.getblogs.net/32218530/how-to-install-windows-11
-* https://mauricemuteti.site123.me/articles/how-to-install-windows-11
-* http://mauricemuteti.inube.com/blog/8795263/how-to-install-windows-11/
-* https://unkbownzero.blog5.net/42811267/how-to-install-windows-11
-* https://unknownzero.blog2learn.com/48895098/how-to-install-windows-11
-* https://penzu.com/p/9754ff41
-* https://hakwpx6nggxq.blog.fc2.com/blog-entry-1.html
-* https://mauricemuteti.edublogs.org/2021/07/02/how-to-install-windows-11/
-* http://mauricemuteti.unblog.fr/2021/07/02/how-to-install-windows-11/
-* http://mauricemuteti.moonfruit.com/how-to-install-windows-11/4595307736
-* http://lxklzkvhlx.jigsy.com/how-to-install-windows-11
-* http://lxklzkvhlx.jigsy.com/entries/general/how-to-install-windows-11-how-to-install-windows-11
-* http://jsdghliasgd.bravesites.com/entries/general/how-to-install-windows-11
-* http://jsdghliasgd.bravesites.com/
-* https://mauricemuteti.webstarts.com/how_to_install_windows_11.html?r=20210702161949
-* https://mauricemuteti.webstarts.com/how_to_install_windows_11.html
-* https://mauricemuteti.webstarts.com/blog/post/how-to-install-windows-11
-* https://oshfoyhfefsf.bravesites.com/
-* http://oshfoyhfefsf.bravesites.com/how-to-install-windows-11
-* http://oshfoyhfefsf.bravesites.com/entries/general/how-to-install-windows-11
-* https://unknown-zero.jimdosite.com/
-* http://mauricemuteti.over-blog.com/2021/07/how-to-install-windows-11.html
-* http://mauricemuteti.wikidot.com/blog:2
-* https://all4webs.com/oishdpaasfj/home.htm?4532=6256
-* http://mauricemuteti.eklablog.com/how-to-install-windows-11-a209195850
-* http://publish.lycos.com/mauricemuteti/2021/07/02/3/
-* https://mauricemuteti2015.wixsite.com/post/about
-* https://hubpages.com/@mauricemuteti
-* https://elgg.org/profile/mauricemuteti
-* https://imgur.com/a/JwAHuNH
-* https://www.slideshare.net/mauricemuteti2015/how-to-install-windows-11
-* https://www.4shared.com/office/giMOKP1Riq/mauricemutetiinfohow-to-instal.html
-* https://www.4shared.com/web/preview/pdf/giMOKP1Riq
-* https://slides.com/mauricemuteti/deck-0a7025
-* https://en.calameo.com/read/0058930621b78c9ee4b3f
-* https://www.slideserve.com/mauricemuteti/how-to-install-windows-11
-* https://www.academia.edu/s/6beb429c23
-* https://www.academia.edu/s/6beb429c23?source=link
-* http://www.authorstream.com/Presentation/mauricemuteti-4638707-install-windows-11/
-* https://www.academia.edu/49526609/How_To_Install_Windows_11
-* https://issuu.com/mauricemuteti2015/docs/mauricemuteti.infohow-to-install-windows-11
-* https://www.scribd.com/document/514108238/How-To-Install-Windows-11
+https://twitter.com/ea_plus/status/1409844229753483264
+https://www.facebook.com/permalink.php?story_fbid=1306067539787545&id=100011529331580
+https://www.facebook.com/mutetimaurice/posts/2866567980274193
+https://www.linkedin.com/posts/learning-tutorials-731a29114_how-to-install-windows-11-activity-6815613787006226432-PKAx
+https://slashdot.org/submission/14081432/how-to-install-windows-11
+https://www.linkedin.com/pulse/how-install-windows-11-learning-tutorials/
+https://www.pinterest.com/pin/94505292172173849
+https://www.instapaper.com/read/1424271694
+https://www.instapaper.com/read/1424271834
+https://dai.ly/x82bx57
+https://neolatino.ning.com/profiles/blogs/how-to-install-windows-11
+https://neolatino.ning.com/profiles/status/show?id=516621%3AStatus%3A23576934
+https://neolatino.ning.com/video/how-to-install-windows-11
+https://vimeo.com/568893449
+https://www.dailymotion.com/video/x82bx57
+http://www.socialbookmarkssite.com/bookmark/3069086/how-to-install-windows-11/
+http://www.bookmark4you.com/user/2976279-mauricemuteti2015
+http://www.video-bookmark.com/bookmark/4474984/how-to-install-windows-11/
+http://www.socialbookmarkssite.com/bookmark/3069120/how-to-install-windows-11/
+http://www.video-bookmark.com/bookmark/4475003/how-to-install-windows-11/
+https://vk.com/id661634275?w=wall661634275_1
+https://vk.com/wall661634275_1
+https://vk.com/id661634275
+https://mauricemuteti.skyrock.com/3342974228-How-To-Install-Windows-11.html
+https://www.reddit.com/user/Sdreloaded/comments/oa8quh/how_to_install_windows_11/
+https://www.taringa.net/+videos/how-to-install-windows-11-article_4483i0
+https://www.minds.com/newsfeed/1256613515154817024?referrer=mauricemuteti
+https://www.gaiaonline.com/profiles/mmaurixe/45550952/
+https://mauricemutetingundi.blogspot.com/2021/06/how-to-install-windows-11.html
+https://mauricemuteti2015.tumblr.com/post/655336267607834624/how-to-install-windows-11-maurice-muteti
+https://mauricemutetingundi.wordpress.com/2021/06/29/how-to-install-windows-11-maurice-muteti/
+https://mauricemuteti2015.medium.com/how-to-install-windows-11-f170cc47999c
+https://b.hatena.ne.jp/entry/panel/?url=https%3A%2F%2Fmauricemuteti.info%2Fhow-to-install-windows-11%2F%23.YNtpPxBI5W4.hatena&btitle=How+To+Install+Windows+11+%E2%80%93+Maurice+Muteti
+https://b.hatena.ne.jp/entry/4704771760354497602/comment/mauricemuteti2015
+https://b.hatena.ne.jp/entry/s/mauricemuteti.info/how-to-install-windows-11/
+https://www.folkd.com/user/mauricemuteti2015
+https://www.folkd.com/user/mauricemuteti2015?filter_content=news&pagenum=1
+https://www.folkd.com/user/mauricemuteti2015?filter_content=pages&pagenum=1
+https://www.scoop.it/topic/how-to-tutorials/p/4125588780/2021/06/29/https-mauricemuteti-info-how-to-install-windows-11
+http://sco.lt/9CRFWC
+https://twitter.com/ea_plus/status/1409960403250130945
+https://www.pinterest.com/pin/94505292172175653
+https://www.facebook.com/permalink.php?story_fbid=1306318636429102&id=100011529331580
+https://mix.com/!uzQxFncQ:how-to-install-windows-11
+https://mix.com/!H8Ede87J:https%3A-mauricemuteti.info-how-to-install-windows-11
+https://twitter.com/ea_plus/status/1409962829696864260
+https://www.pinterest.com/pin/94505292172175691
+https://nk.pl/#profil/53355945/
+https://www.bibsonomy.org/url/7b3f6fb68f59f5f93618391a6b4de543
+https://www.bookmarky.cz/users/mauricemuteti
+https://www.diigo.com/user/mauricemuteti
+https://diigo.com/0l1ywz
+https://www.diigo.com/annotated/2e09530c3da71c3363f021e97f28a211
+https://www.liveinternet.ru/users/mauricemuteti/post485154054//
+https://www.pusha.se/how-to-install-windows-11
+https://www.pusha.se/how-to-install-windows-11-2
+https://www.plurk.com/p/og0i2r
+https://www.plurk.com/p/og0i4f
+https://flip.it/b0Eyjx
+https://www.google.com/url?q=https://mauricemuteti.info/how-to-install-windows-11/%23.YN8A2PR8aN0.google&usd=2&usg=AFQjCNEQsIp3BMNaJliZuxGUSdrOwROr7g
+https://sites.google.com/site/mauricemutetingundi2015/how-to-install-windows-11
+https://b.hatena.ne.jp/entry?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DiRt5VnxSA6o
+https://mauricemuteti.livejournal.com/1787.html
+https://www.evernote.com/shard/s581/sh/9260e90c-34f5-fa68-1c84-326141d126fc/38e464129bf93124e14cd5aa2abb8c7c
+https://posteezy.com/how-install-windows-11-0
+https://digg.com/news/link/how-to-install-windows-11-xPxRrhDZq1
+https://digg.com/@maurice-muteti
+https://www.smore.com/yqutg-my-smore-newsletter
+http://mauricemuteti.zohosites.com/blogs/post/how-to-install-windows-11
+https://mauricemuteti.page.tl/How-To-Install-Windows-11.htm
+https://unknowzeros-website.yolasite.com/how-to-install-windows-11/
+https://mauricemuteti.webs.com/apps/blog/
+https://mauricemuteti.webs.com/apps/blog/show/49924387-how-to-install-windows-11
+https://mauricemuteti.shutterfly.com/howtoinstallwindows11
+https://mauricemuteti.shutterfly.com/howtoinstallwindows11/2
+https://mauricemuteti.shutterfly.com/howtoinstallwindows11/3
+https://mauricemuteti.webnode.com/accommodation/
+https://unknownzero.getblogs.net/32218530/how-to-install-windows-11
+https://mauricemuteti.site123.me/articles/how-to-install-windows-11
+http://mauricemuteti.inube.com/blog/8795263/how-to-install-windows-11/
+https://unkbownzero.blog5.net/42811267/how-to-install-windows-11
+https://unknownzero.blog2learn.com/48895098/how-to-install-windows-11
+https://penzu.com/p/9754ff41
+https://hakwpx6nggxq.blog.fc2.com/blog-entry-1.html
+https://mauricemuteti.edublogs.org/2021/07/02/how-to-install-windows-11/
+http://mauricemuteti.unblog.fr/2021/07/02/how-to-install-windows-11/
+http://mauricemuteti.moonfruit.com/how-to-install-windows-11/4595307736
+http://lxklzkvhlx.jigsy.com/how-to-install-windows-11
+http://lxklzkvhlx.jigsy.com/entries/general/how-to-install-windows-11-how-to-install-windows-11
+http://jsdghliasgd.bravesites.com/entries/general/how-to-install-windows-11
+http://jsdghliasgd.bravesites.com/
+https://mauricemuteti.webstarts.com/how_to_install_windows_11.html?r=20210702161949
+https://mauricemuteti.webstarts.com/how_to_install_windows_11.html
+https://mauricemuteti.webstarts.com/blog/post/how-to-install-windows-11
+https://oshfoyhfefsf.bravesites.com/
+http://oshfoyhfefsf.bravesites.com/how-to-install-windows-11
+http://oshfoyhfefsf.bravesites.com/entries/general/how-to-install-windows-11
+https://unknown-zero.jimdosite.com/
+http://mauricemuteti.over-blog.com/2021/07/how-to-install-windows-11.html
+http://mauricemuteti.wikidot.com/blog:2
+https://all4webs.com/oishdpaasfj/home.htm?4532=6256
+http://mauricemuteti.eklablog.com/how-to-install-windows-11-a209195850
+http://publish.lycos.com/mauricemuteti/2021/07/02/3/
+https://mauricemuteti2015.wixsite.com/post/about
+https://hubpages.com/@mauricemuteti
+https://elgg.org/profile/mauricemuteti
+https://imgur.com/a/JwAHuNH
+https://www.slideshare.net/mauricemuteti2015/how-to-install-windows-11
+https://www.4shared.com/office/giMOKP1Riq/mauricemutetiinfohow-to-instal.html
+https://www.4shared.com/web/preview/pdf/giMOKP1Riq
+https://slides.com/mauricemuteti/deck-0a7025
+https://en.calameo.com/read/0058930621b78c9ee4b3f
+https://www.slideserve.com/mauricemuteti/how-to-install-windows-11
+https://www.academia.edu/s/6beb429c23
+https://www.academia.edu/s/6beb429c23?source=link
+http://www.authorstream.com/Presentation/mauricemuteti-4638707-install-windows-11/
+https://www.academia.edu/49526609/How_To_Install_Windows_11
+https://issuu.com/mauricemuteti2015/docs/mauricemuteti.infohow-to-install-windows-11
+https://www.scribd.com/document/514108238/How-To-Install-Windows-11
  */
 package javaapplication456;
 
